ba529e60f7c9e750098fa7764d66f87d0d92299f
Bobby Bouwmann <bobbybouwmann@gmail.com>
2015-02-20 13:20:02 +0000

    Smooth the migration a but up

--- src/migrations/2015_02_19_102113_create_entry_tables.php
+++ src/migrations/2015_02_19_102113_create_entry_tables.php
@@ -15,7 +15,6 @@ class CreateEntryTables extends Migration {
         Schema::table('users', function(Blueprint $table)
         {
             $table->integer('role_id')->unsigned();
-//            $table->foreign('role_id')->references('id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
         });
 
         Schema::create('roles', function(Blueprint $table)
@@ -38,10 +37,7 @@ class CreateEntryTables extends Migration {
 
         Schema::create('permission_role', function (Blueprint $table) {
             $table->string('permission_name');
-//            $table->foreign('permission_name')->references('name')->on('permissions')->onUpdate('cascade')->onDelete('cascade');
-
             $table->integer('role_id')->unsigned();
-//            $table->foreign('role_id')->references('id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
 
             $table->primary(['permission_name', 'role_id']);
         });
@@ -57,11 +53,9 @@ class CreateEntryTables extends Migration {
         Schema::table('users', function(Blueprint $table)
         {
             $table->dropColumn('role_id');
-//            $table->dropForeign('users_role_id_foreign');
         });
 
         Schema::drop('roles');
-        Schema::drop('role_user');
         Schema::drop('permissions');
         Schema::drop('permission_role');
     }
